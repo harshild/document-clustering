4bf529dd2848dfc7d5af1562e740b1aaac77e2f2
harshild <harshil.dhariwal@gmail.com>
2020-05-09 19:59:24 -0400

    Added handling for encoding types when reading file

--- document-clustering.py
+++ document-clustering.py
@@ -1,11 +1,14 @@
 import os
+import html2text
+from pip._vendor.chardet import detect
 
 
 def main():
-    for filename in sorted(os.listdir(os.curdir+"/files/")):
-        with open(os.path.join(os.curdir,"./files/", filename), 'r') as f:
-            print(filename)
-
+    input_file_list = os.listdir(os.curdir + "/files/")
+    for filename in sorted(input_file_list):
+        with open("./files/"+ filename, 'r',encoding="latin-1", errors="surrogateescape") as f:
+            print(f.name)
+            print(f.read())
 
 
 if __name__ == '__main__':
