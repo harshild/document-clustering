06190d65f20c15774938984558eebb5c7822b699
harshild <harshil.dhariwal@gmail.com>
2020-05-09 21:04:11 -0400

    Writing Tokens to file Replaced line break with spaces Replaced tabs with spaces

--- document-clustering.py
+++ document-clustering.py
@@ -5,14 +5,24 @@ import html2text
 
 def get_tokens(file_data):
     token_dict = {}
+    file_data = file_data.replace("\n", " ")
+    file_data = file_data.replace("\t", " ")
     for token in file_data.split(" "):
         stripped_token = token.strip()
         if stripped_token != "":
             if token_dict.keys().__contains__(stripped_token):
                 token_dict[stripped_token] = token_dict[stripped_token] + 1
-            else :
+            else:
                 token_dict[stripped_token] = 1
     return token_dict
+
+
+def write_tokens_to_file(file_name, sorted_by, tokens_list):
+    filename = "./output/" + file_name + "_" + sorted_by + ".txt"
+
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    for t, f in tokens_list:
+        open(filename, "a+").write(t + " , " + str(f) + "\n")
 
 
 def main():
@@ -23,8 +33,10 @@ def main():
             file_data = get_file_data(f.read())
             file_tokens = get_tokens(file_data)
 
-            print(sorted(file_tokens.items(), key=lambda kv: (kv[0], kv[1])))
-            print(sorted(file_tokens.items(), key=lambda kv: (kv[1], kv[0])))
+            file_tokens_sorted_key = sorted(file_tokens.items(), key=lambda tf: (tf[0], tf[1]))
+            write_tokens_to_file(filename.split(".html")[0], "token", file_tokens_sorted_key)
+            file_tokens_sorted_count = sorted(file_tokens.items(), key=lambda tf: (tf[1], tf[0]))
+            write_tokens_to_file(filename.split(".html")[0], "frequency", file_tokens_sorted_count)
 
 
 def get_file_data(html_data):
