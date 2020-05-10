a406bb0189c18df09a3eac1b879a7d542501c9a8
harshild <harshil.dhariwal@gmail.com>
2020-05-09 20:42:30 -0400

    Tokenized text Sorted on basis of token sorted on basis of token occurrence count

--- document-clustering.py
+++ document-clustering.py
@@ -1,15 +1,30 @@
 import os
+
 import html2text
-from pip._vendor.chardet import detect
+
+
+def get_tokens(file_data):
+    token_dict = {}
+    for token in file_data.split(" "):
+        stripped_token = token.strip()
+        if stripped_token != "":
+            if token_dict.keys().__contains__(stripped_token):
+                token_dict[stripped_token] = token_dict[stripped_token] + 1
+            else :
+                token_dict[stripped_token] = 1
+    return token_dict
 
 
 def main():
     input_file_list = os.listdir(os.curdir + "/files/")
-    for filename in sorted(input_file_list):
+    for filename in sorted(input_file_list[:]):
         with open("./files/" + filename, 'r', encoding="latin-1", errors="surrogateescape") as f:
             print(f.name)
             file_data = get_file_data(f.read())
-            print(file_data)
+            file_tokens = get_tokens(file_data)
+
+            print(sorted(file_tokens.items(), key=lambda kv: (kv[0], kv[1])))
+            print(sorted(file_tokens.items(), key=lambda kv: (kv[1], kv[0])))
 
 
 def get_file_data(html_data):
