7d7900ab6865b0f4985f1af68a9d4e62857357cb
harshild <harshil.dhariwal@gmail.com>
2020-05-09 20:16:14 -0400

    Extracting data from HTML file using HTML to Text library

--- document-clustering.py
+++ document-clustering.py
@@ -6,9 +6,16 @@ from pip._vendor.chardet import detect
 def main():
     input_file_list = os.listdir(os.curdir + "/files/")
     for filename in sorted(input_file_list):
-        with open("./files/"+ filename, 'r',encoding="latin-1", errors="surrogateescape") as f:
+        with open("./files/" + filename, 'r', encoding="latin-1", errors="surrogateescape") as f:
             print(f.name)
-            print(f.read())
+            file_data = get_file_data(f.read())
+            print(file_data)
+
+
+def get_file_data(html_data):
+    h = html2text.HTML2Text()
+    data = h.handle(html_data)
+    return data
 
 
 if __name__ == '__main__':
